99da4e5a610b1832d3aebab2af5dcfa22bb4bacc
khan <Cstyler>
2015-12-26 21:55:38 +0300

    upd

--- snake.py
+++ snake.py
@@ -20,7 +20,8 @@ class Board(object):
             for y in xrange(self.board_height):
                 self.matrix[x][y] = self.canv.create_rectangle(x*cell_size+cell_distance, \
                 y*cell_size+cell_distance, (x+1)*cell_size-cell_distance, \
-                (y+1)*cell_size-cell_distance, state = HIDDEN, fill = 'green', outline = 'green')
+                (y+1)*cell_size-cell_distance, state = HIDDEN, fill = 'green', outline = 'green', tags=('%d' % x, '%d' % y))
+
     def draw_cell(self,event):
         """Make cells visible by clicking on left mouse button."""
         try:
@@ -30,43 +31,66 @@ class Board(object):
             self.canv.itemconfig(cell, state = NORMAL)
         except IndexError:
             pass
+
     def get(self):
-        return (self.visible_cells, self.canv, self.board_width, self.board_height)
+        return (self.visible_cells, self.canv, self.matrix, self.board_width, self.board_height)
 
 class Snake(object):
     """Move snake and stop it, if game is over."""
     def __init__(self, board, repeat_time=0.1):
-        self.body, self.canv, board_width, board_height = board.get()
+        self.body, self.canv, self.matrix, board_width, board_height = board.get()
         self.timer = None
         self.repeat_time = repeat_time
-        self.range = board_width * board_height
+        self.range = range(1, board_width * board_height+1)
         self.is_running = False
-        self.food = choice(xrange(1, self.range+1))
+        self.food = choice(self.range)
         self.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
+        self.fline, self.eline = [], []
+        for i in self.matrix:
+            self.fline.append(i[0])
+            self.eline.append(i[len(i)-1])
+
     def move(self, next_coord):
         self.canv.itemconfig(self.body[0], state = HIDDEN)
         head = self.body[len(self.body)-1] + next_coord
+        if head-next_coord in self.fline and self.eline[self.fline.index(head-next_coord)] not in self.body:
+            head -= next_coord
+            head = self.eline[self.fline.index(head)]
+        elif head-next_coord in self.eline and self.fline[self.eline.index(head-next_coord)] not in self.body:
+            head -= next_coord
+            head = self.fline[self.eline.index(head)]
+        if head not in self.range:
+            head -= next_coord
+            if head in self.matrix[0]:
+                head = self.matrix[self.matrix.shape[0]-1][list(self.matrix[0]).index(head)]
+            else:
+                head = self.matrix[0][list(self.matrix[self.matrix.shape[0]-1]).index(head)]
         if head in self.body:
-            self.stop()
-            game_over = Tk()
-            game_over.geometry('+750+150')
-            Label(game_over, text="Game Over!").pack()
-            game_over.mainloop()
+            self.stop_game()
             return
         self.canv.itemconfig(head, state = NORMAL, fill = 'green', outline = 'green')
         self.body.append(head)
         if head == self.food:
-            self.food = choice(filter(lambda x: x not in self.body, xrange(1, self.range+1)))
+            self.food = choice(filter(lambda x: x not in self.body, self.range))
             self.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
         else:
             self.body = self.body[1:len(self.body)]
         self.is_running = True
         self.timer = Timer(self.repeat_time, lambda: self.move(next_coord))
         self.timer.start()
+
     def stop(self):
         self.timer.cancel()
+
     def set_level(self, time):
         self.repeat_time = time
+
+    def stop_game(self):
+        self.stop()
+        game_over = Tk()
+        game_over.geometry('+750+150')
+        Label(game_over, text="Game Over!").pack()
+        game_over.mainloop()
 
 def restart(root, snake):
     if snake.is_running:
@@ -85,7 +109,7 @@ def main():
     cell_distance = 1
     win_height = cell_size*30
     win_width = cell_size*40
-    root.geometry("%sx%s+500+0" % (str(win_width+58), str(win_height+5)))
+    root.geometry("%dx%d+500+0" % (win_width+58, win_height+5))
     frame = Frame(root, bd = 2, bg = 'black')
     board = Board(frame, cell_size, win_height, win_width, cell_distance)
     snake = Snake(board)
@@ -109,4 +133,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
