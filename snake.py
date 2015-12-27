05be818dc756db111457c57e56461cd783004b32
khan <Cstyler>
2015-12-27 17:29:21 +0300

    upd

--- snake.py
+++ snake.py
@@ -2,7 +2,8 @@
 from Tkinter import Tk, Button, Frame, Canvas, Label, Menu, NORMAL, HIDDEN, BOTH
 from numpy import zeros, int16
 from threading import Timer
-from random import choice
+from random import randint, choice
+from itertools import product
 
 class Board(object):
     """Creates board with hidden cells."""
@@ -25,58 +26,58 @@ class Board(object):
     def draw_cell(self,event):
         """Make cells visible by clicking on left mouse button."""
         try:
-            cell = self.matrix[(event.x-self.cell_distance)/self.cell_size][(event.y-self.cell_distance)/self.cell_size]
-            if cell not in self.visible_cells:
-                self.visible_cells.append(cell)
-            self.canv.itemconfig(cell, state = NORMAL)
+            x = (event.x-self.cell_distance)/self.cell_size
+            y = (event.y-self.cell_distance)/self.cell_size
+            if (x, y) not in self.visible_cells:
+                self.visible_cells.append((x,y))
+            self.canv.itemconfig(self.matrix[x][y], state = NORMAL)
         except IndexError:
             pass
-
-    def get(self):
-        return (self.visible_cells, self.canv, self.matrix, self.board_width, self.board_height)
 
 class Snake(object):
     """Move snake and stop it, if game is over."""
     def __init__(self, board, repeat_time=0.1):
-        self.body, self.canv, self.matrix, board_width, board_height = board.get()
+        self.body = board.visible_cells
+        self.board = board
         self.timer = None
         self.repeat_time = repeat_time
-        self.range = range(1, board_width * board_height+1)
         self.is_running = False
-        self.food = choice(self.range)
-        self.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
-        self.fline, self.eline = [], []
-        for i in self.matrix:
-            self.fline.append(i[0])
-            self.eline.append(i[len(i)-1])
-
-    def move(self, next_coord):
-        self.canv.itemconfig(self.body[0], state = HIDDEN)
-        head = self.body[len(self.body)-1] + next_coord
-        if head-next_coord in self.fline and self.eline[self.fline.index(head-next_coord)] not in self.body:
-            head -= next_coord
-            head = self.eline[self.fline.index(head)]
-        elif head-next_coord in self.eline and self.fline[self.eline.index(head-next_coord)] not in self.body:
-            head -= next_coord
-            head = self.fline[self.eline.index(head)]
-        if head not in self.range:
-            head -= next_coord
-            if head in self.matrix[0]:
-                head = self.matrix[self.matrix.shape[0]-1][list(self.matrix[0]).index(head)]
-            else:
-                head = self.matrix[0][list(self.matrix[self.matrix.shape[0]-1]).index(head)]
-        if head in self.body:
+        self.product = list(product(xrange(0, self.board.board_width), xrange(0, self.board.board_height)))
+        self.food = self.board.matrix[randint(0, self.board.board_width-1)][randint(0, self.board.board_height-1)]
+        self.board.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
+    def move(self, tag):
+        x, y = self.body[0]
+        self.board.canv.itemconfig(self.board.matrix[x][y], state = HIDDEN)
+        x, y = self.body[len(self.body) - 1]
+        if tag == 'u':
+            y -= 1
+            y %= self.board.matrix.shape[1]
+            head = self.board.matrix[x][y]
+        elif tag == 'd':
+            y += 1
+            y %= self.board.matrix.shape[1]
+            head = self.board.matrix[x][y]
+        elif tag == 'r':
+            x += 1
+            x %= self.board.matrix.shape[0]
+            head = self.board.matrix[x][y]
+        elif tag == 'l':
+            x -= 1
+            x %= self.board.matrix.shape[0]
+            head = self.board.matrix[x][y]
+        if (x, y) in self.body:
             self.stop_game()
             return
-        self.canv.itemconfig(head, state = NORMAL, fill = 'green', outline = 'green')
-        self.body.append(head)
+        self.board.canv.itemconfig(head, state = NORMAL, fill = 'green', outline = 'green')
+        self.body.append((x, y))
         if head == self.food:
-            self.food = choice(filter(lambda x: x not in self.body, self.range))
-            self.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
+            x, y = choice(filter(lambda x: x not in self.body, self.product))
+            self.food = self.board.matrix[x][y]
+            self.board.canv.itemconfig(self.food, state = NORMAL, fill = 'red', outline = 'red')
         else:
             self.body = self.body[1:len(self.body)]
         self.is_running = True
-        self.timer = Timer(self.repeat_time, lambda: self.move(next_coord))
+        self.timer = Timer(self.repeat_time, lambda: self.move(tag))
         self.timer.start()
 
     def stop(self):
@@ -107,8 +108,8 @@ def main():
     root = Tk()
     cell_size = 20
     cell_distance = 1
-    win_height = cell_size*30
-    win_width = cell_size*40
+    win_height = cell_size*15
+    win_width = cell_size*20
     root.geometry("%dx%d+500+0" % (win_width+58, win_height+5))
     frame = Frame(root, bd = 2, bg = 'black')
     board = Board(frame, cell_size, win_height, win_width, cell_distance)
@@ -121,10 +122,10 @@ def main():
     menubar.add_cascade(label="Level", menu=levelmenu)
     root.config(menu=menubar)
     btn1 = Button(frame, text = 'Reboot', width = 3, command = lambda: restart(root, snake))
-    root.bind('<Right>', lambda x: move(snake, win_height/cell_size))
-    root.bind('<Left>', lambda x: move(snake, -win_height/cell_size))
-    root.bind('<Down>', lambda x: move(snake, 1))
-    root.bind('<Up>', lambda x: move(snake, -1))
+    root.bind('<Right>', lambda x: move(snake, 'r'))
+    root.bind('<Left>', lambda x: move(snake, 'l'))
+    root.bind('<Down>', lambda x: move(snake, 'd'))
+    root.bind('<Up>', lambda x: move(snake, 'u'))
     btn1.pack(side = 'right')
     frame.pack()
     board.canv.pack(fill= BOTH)
